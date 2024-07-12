ed5f721a32530a2ceb1baadb2e2d34023aa7447f
Varun Pandey <varunp5324@gmail.com>
2024-07-12 17:11:03 +0530

    adding files

--- src/Project.jsx
+++ src/Project.jsx
@@ -28,7 +28,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/x6BhKcC/Animation.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Mini-Front-End-Projects.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -36,7 +36,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/7YwsKps/portfolio.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    <a href='https://portfolio-y3a5.onrender.com/' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
                     <a href='https://github.com/Varunp01/Portfolio.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -44,7 +44,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/0GxkrbC/tic-tac-toe.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    <a href='https://tic-tac-toe-react-7ywz.onrender.com/' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
                     <a href='https://github.com/Varunp01/Tic-Tac-Toe-React.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -52,7 +52,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/cK9nhqL/Task-Wave.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Mini-Front-End-Projects/tree/73e17b3a532f44018414d1ae702c4ba16a31a1ea/To-Do-List' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -60,7 +60,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/5TLKL6q/Spotify-Clone.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Major-Front-End-Projects/tree/main/1-Spotify%20clone' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -68,7 +68,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/cwH3MKZ/Weather-App.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Major-Front-End-Projects/tree/main/2-Weather%20App' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -76,7 +76,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/WKb4LyS/Animal-Factory.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Animal-Factory.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -84,7 +84,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/610sBdR/Bookshelf.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    <a href='https://personal-bookshelf-80we.onrender.com/' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
                     <a href='https://github.com/Varunp01/Personal-Bookshelf.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -92,7 +92,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/FWr0yFP/React-Quiz.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    {/* <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a> */}
                     <a href='https://github.com/Varunp01/Quiz-react.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
@@ -100,7 +100,7 @@ export default function Project() {
                     <div className="contentImg"><img src="https://i.ibb.co/ZNth2Xg/wanderlust.png"></img></div>
                     <br></br>
                     <div className="icons">
-                    <a href='' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
+                    <a href='https://github.com/Varunp01/WanderLust-Project.git' className='LIVE'><FontAwesomeIcon icon={faMobileButton} color="black"></FontAwesomeIcon></a>
                     <a href='https://github.com/Varunp01/WanderLust-Project.git' className='GITHUB'><FontAwesomeIcon icon={faGithub} color="black"></FontAwesomeIcon></a>
                     </div>
                 </div>
